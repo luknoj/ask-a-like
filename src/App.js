cd8aa0cf7dc8970307e1462d53127aa03bc7c735
mighty-titan <30566936+mighty-titan@users.noreply.github.com>
2017-08-23 12:04:11 +0200

    Add files via upload
    Added all files, in src file for develop

---
+++ src/App.js
@@ -0,0 +1,288 @@
+import React, { Component } from 'react';
+import './css/bootstrap.css';
+import './css/my_app.css';
+import firebase from './firebase.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      comments: [],
+    };
+  }
+
+  handleSubmitPost(e) {
+    e.preventDefault();
+    const postsRef = firebase.database().ref('posts');
+
+    // Assign all Form elements to variable.
+    // e.target is a element that triggered the event, which is form in this case.
+    const inputs = e.target.elements;
+
+    postsRef.push({
+      postContent: inputs.postContent.value,
+      postUsername: inputs.postUsername.value,
+      postTitle: inputs.postTitle.value,
+      postDate: new Date().toUTCString(),
+    });
+
+    // Clear all form inputs.
+    e.target.reset();
+  }
+
+  handleSubmitComment(e, post) {
+    e.preventDefault();
+    const commentsRef = firebase.database().ref('comments');
+
+    // Assign all Form elements to variable.
+    // e.target is a element that triggered the event, which is form in this case.
+    const inputs = e.target.elements;
+
+    commentsRef.push({
+      postId: post.postId,
+      commentContent: inputs.commentContent.value,
+      commentUsername: inputs.commentUsername.value,
+      commentDate: new Date().toUTCString(),
+    });
+
+    // Clear all form inputs.
+    e.target.reset();
+  }
+
+  componentDidMount() {
+    const postsRef = firebase.database().ref('posts');
+
+      postsRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      const posts = [];
+
+      for (const key in data) {
+        posts.push({
+          id: key,
+          postId: key,
+          postTitle: data[key].postTitle,
+          postContent: data[key].postContent,
+          postUsername: data[key].postUsername,
+          postDate: data[key].postDate,
+        });
+      }
+      posts.reverse();
+      this.setState({ posts });
+    });
+    const commentRef = firebase.database().ref('comments');
+
+    commentRef.on('value', (snapshot) =>{
+      const data = snapshot.val();
+      const comments = [];
+
+      for(const key in data) {
+        comments.push({
+          id: key,
+          postId: data[key].postId,
+          commentContent: data[key].commentContent,
+          commentUsername: data[key].commentUsername,
+          commentDate: data[key].commentDate,
+        });
+      }
+      comments.reverse();
+      this.setState({ comments });
+    })
+  }
+
+  removeItemComment(postId) {
+    const commentsRef = firebase.database().ref(`/comments/${postId}`);
+    commentsRef.remove();
+  }
+
+  removeItemPost(postId) {
+    const postsRef = firebase.database().ref(`/posts/${postId}`);
+    postsRef.remove();
+  }
+
+
+
+  render() {
+    return (
+      <div className='container-fluid'>
+        <header>
+          <div className='row justify-content-center'>
+            <div className='col-sm-12'>
+              <h1 className="display-2">ASK A LIKE</h1>
+            </div>
+          </div>
+        </header>
+        {/* ::::::::::::::::::NAWIGACJA:::::::::::::::::: */}
+
+        <div className="row">
+          <div className="col-sm-12">
+            <ul className="nav nav-pills justify-content-center">
+              <li className="nav-item">
+                <a className="nav-link" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Posts</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* ::::::::::::::::::WPROWADZANIE POSTOW::::::::::::::::::*/}
+
+        <div className='row justify-content-center'>
+          <div className='col-md-3'>
+            <section id="Add post">
+              <form onSubmit={(e) => this.handleSubmitPost(e)}>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input type="text"
+                         className="form-control"
+                         name="postUsername"
+                         placeholder="name"
+                         required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="title">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="postTitle"
+                    placeholder="Title"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="text">Input your question here!</label>
+                  <textarea
+                    type="text"
+                    className="form-control"
+                    name="postContent"
+                    placeholder="question"
+                  />
+                </div>
+                <div className="form-group">
+                  <button className='add-post'>Add Item</button>
+                </div>
+              </form>
+            </section>
+          </div>
+
+
+          {/* ::::::::::::::::::WYSWIETLANIE POSTOW i KOMENTARZY::::::::::::::::::*/}
+
+          {/*::::::::::::::::::WYSWIETLANIE POSTOW::::::::::::::::::*/}
+          <div className='margin-t col-sm-6'>
+            <section className='display-item'>
+              <div>
+                <h1 className="text-center">Newest Posts</h1>
+                  {this.state.posts.map((post) => {
+                    {/*console.log('kurna:' + post.id);*/}
+                    return (
+                      <div className="margin-b card">
+                          <div className="card-header">
+                            <div className="row">
+                              <div className="col-md-6">
+                                <strong>{post.postTitle}</strong>
+                              </div>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                        <div className="col post-content rounded margin-b">
+                          <p className="card-text">{post.postContent}</p>
+                        </div>
+                          {/*::::::::::::::::::USUWANIE POSTA::::::::::::::::::*/}
+                          <div className="row">
+                            <div className="col-md-6">
+                              <button className="btn btn-primary" onClick={() => this.removeItemPost(post.id)}>Remove post
+                              </button>
+                            </div>
+
+                            <div className="col-md-6">
+                              <footer className="blockquote-footer margin-b text-right">Author: {post.postUsername}
+                                <br />{post.postDate}</footer>
+                            </div>
+                          </div>
+                        </div>
+
+                        {/*::::::::::::::::::WYSWIETLANIE KOMENTARZY::::::::::::::::::*/}
+                        <div className="card-body">
+                          <section id="display-comment">
+                            <div className="row align-items-top justify-content-center">
+                              <div className="col">
+                              {this.state.comments.map((comment) => {
+                                if(post.postId == comment.postId)
+                                {
+                                return(
+                                    <div className="row">
+                                    <div className="col-md-11 comment margin-b rounded">
+                                      <p className="card-text text-justify margin-t">
+                                        {comment.commentContent}
+                                      </p>
+                                      <p className="blockquote-footer text-right">
+                                        {comment.commentUsername}
+                                        <br/>
+                                        {comment.commentDate}
+                                      </p>
+                                    </div>
+                                    <div className="col-md-1">
+                                    <button
+                                      className="btn btn-primary" onClick={() => this.removeItemComment(comment.id)}>X</button>
+                                    </div>
+                                    </div>
+                                );
+                              };
+                            })}
+                              </div>
+                            </div>
+                          </section>
+                        </div>
+                        {/*WPROWADZANIE KOMENTARZA*/}
+
+                        <div className="card-body">
+                          <section id="Add comment">
+                            <form onSubmit={(e) => this.handleSubmitComment(e, post)}>
+                              <div className="row align-items-top">
+                                <div className="col-md-2">
+                                  <input
+                                    type="text"
+                                    className="form-control"
+                                    name="commentUsername"
+                                    placeholder="name"
+                                    required
+                                  />
+                                  <button className='btn btn-primary margin-t size-go'>GO!</button>
+                                </div>
+
+                                <div className="col-md-10">
+                                  <textarea
+                                    rows="6"
+                                    type="text"
+                                    className="form-control"
+                                    name="commentContent"
+                                    placeholder="Your post"
+                                    required />
+                                </div>
+                              </div>
+                            </form>
+                          </section>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+
+//<script src="https://www.gstatic.com/firebasejs/4.3.0/firebase.js"></script>
+
+  // Initialize Firebase
+  import firebase from 'firebase'
+  var config = {
+    apiKey: "AIzaSyDkXQkCBHaDfVKxXo1fcyDD2qGWSbbqOAc",
+    authDomain: "fun-food-friends-c48e4.firebaseapp.com",
+    databaseURL: "https://fun-food-friends-c48e4.firebaseio.com",
+    projectId: "fun-food-friends-c48e4",
+    storageBucket: "fun-food-friends-c48e4.appspot.com",
+    messagingSenderId: "695533662983"
+  };
+  firebase.initializeApp(config);
+  export default firebase;
